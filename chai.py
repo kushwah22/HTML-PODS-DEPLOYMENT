10d9d69afa0ae65bd9914afb4e7ba20ed55699fd
Kartik Kushwah <kushwahkartik252@gmail.com>
2025-03-20 23:03:19 +0530

    Add files via upload

---
+++ chai.py
@@ -0,0 +1,3 @@
+from hello import lunch
+
+lunch("ginger tea")
---
+++ hello.py
@@ -0,0 +1,12 @@
+print ("chai or python")
+
+def chai(n):
+    print(n)
+    
+chai("lemon tea and there are good for us")
+
+def lunch(n):
+    print(n)
+    
+lunch("there are many option in the lunch")
+
